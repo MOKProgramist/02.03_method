939deebc5ba3c008fb21dbb9e8c2ae66b0d03ac3
Hi, I'm ANDProgrammist ! <92784790+MOKProgramist@users.noreply.github.com>
2022-03-14 22:17:13 +0300

    Add files via upload

---
+++ the arithmetic mean of two numbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace the_arithmetic_mean_of_two_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число => " );
+            int oneNum = int.Parse(Console.ReadLine());
+            Console.Write("Введите первое число => ");
+            int twoNum = int.Parse(Console.ReadLine()); 
+
+            double arithmeticSum = getArithmeticMean(oneNum, twoNum);
+            Console.WriteLine($"Среднее арифметиеское цифр чисел = {Math.Round(arithmeticSum, 2)}");
+        }
+
+        // количество цифр в числе
+        static int getCountNumberOfNumber(int value, int result = 0)
+        {
+            result++;
+            value /= 10;
+            if (value > 0)
+            {
+                // рекурсия
+                result += getCountNumberOfNumber(value);
+            };
+
+            return result;
+        }
+        // сумма цифр числа
+        static int SumNumber(int value)
+        {
+            if (value <= 10) return value;
+            int result = 0;
+            result += value % 10;
+            value /= 10;
+            if (value > 0)
+            {
+                // рекурсия
+                result += SumNumber(value);
+            };
+
+            return result;
+        }
+        static double getArithmeticMean(int a, int b)
+        {
+            // считаем сумма цифр числа
+            double arifmetNum = ((SumNumber(a)/ getCountNumberOfNumber(a)) + (SumNumber(b) / getCountNumberOfNumber(b)))/2;
+            // Console.WriteLine($"getCountNumberOfNumber a = {getCountNumberOfNumber(a)}\ngetCountNumberOfNumber b = {getCountNumberOfNumber(b)}");
+
+            return arifmetNum;
+        }
+    }
+}
---
+++ the number of roots of the level/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace the_number_of_roots_of_the_level
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Введите значение а: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write($"Введите значение b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write($"Введите значение c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            int countKorni = getCountKorni(a: a, b: b, c: c);
+            Console.WriteLine($"В данном уравнение {(countKorni != 0 ? $"{countKorni} корня" : "корней нет")}");
+        }
+
+        static int getCountKorni(int a, int b, int c)
+        {
+            // по формлуле
+            double D;
+            D = b * b - 4 * a * a;
+
+            int result = 0;
+            if (D < 0) {
+                result = 0;
+            }
+            if (D == 0) {
+                result = 1;
+            }
+            if (D > 0) {
+                result = 2;
+            }
+            return result;
+        }
+    }
+}
